f8a1edf20f71960e66c9aec8906b4e667a632785
Sonal Singh <sonal1201kumar@gmail.com>
2024-03-21 00:23:07 +0530

    Feat: Added basic express with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server.config');
+
+const app = express();
+
+//Body-Parsers->Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/ping', (req, res) => {
+    res.send({ msg: "ping cnd is Working properly" })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at: ${PORT}`);
+});
