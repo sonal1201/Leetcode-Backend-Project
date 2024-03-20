9e52ec598b054bbe19823e8613f84778f705c9a2
Sonal Singh <sonal1201kumar@gmail.com>
2024-03-21 02:27:30 +0530

    feat: Added routing struct for verion api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,32 @@
+function pingController(req, res) {
+    res.send({ msg: "Ping controller checked" });
+}
+
+function addproblem(req, res) {
+
+}
+
+function getproblem(req, res) {
+
+}
+
+function getproblems(req, res) {
+
+}
+
+function deleteproblem(req, res) {
+
+}
+
+function updateproblem(req, res) {
+
+}
+
+module.exports = {
+    addproblem,
+    getproblem,
+    getproblems,
+    deleteproblem,
+    updateproblem,
+    pingController
+}
---
+++ src/routes/v1/problems.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { problemController } = require('../../controllers');
+
+const problemRouter = express.Router();
+
+// If any request comes and route continues with /ping, we map it to pingProblemController
+problemRouter.get('/ping', problemController.pingController);
+
+problemRouter.get('/:id', problemController.getproblem);
+
+problemRouter.get('/', problemController.getproblems);
+
+problemRouter.post('/', problemController.addproblem);
+
+problemRouter.delete('/:id', problemController.deleteproblem);
+
+problemRouter.put('/:id', problemController.updateproblem);
+
+
+module.exports = problemRouter; 
