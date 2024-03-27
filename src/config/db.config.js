cc261294accae9e47ffdf3f6dc04127d1d6eb264
Sonal Singh <sonal1201kumar@gmail.com>
2024-03-27 16:25:24 +0530

    Feat: Added Mongoose db config

---
+++ src/config/db.config.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+const { ATLAS_DB_URL, NODE_ENV } = require('./server.config');
+async function connectToDB() {
+    try {
+        if (NODE_ENV == "DEVELOPMENT") {
+            await mongoose.connect(ATLAS_DB_URL);
+        }
+    } catch (error) {
+        console.log("Unable to connect to the DB server");
+        console.log(error);
+    }
+}
+module.exports = connectToDB;
