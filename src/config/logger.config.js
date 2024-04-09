5508059471e8aa2ecc58380e919d85ec854e9a11
Sonal Singh <sonal1201kumar@gmail.com>
2024-04-10 03:11:14 +0530

    Feat: configured logging for file,console and mongo

---
+++ src/config/logger.config.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+const { LOG_DB_URL } = require('./server.config');
+require('winston-mongodb')
+
+const allowedTransport = [];
+//winston.transports.Console() -> configure console based printing inside winston log
+//Logging on the console. 
+allowedTransport.push(new winston.transports.Console({
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH-mm-ss'
+        }),
+        winston.format.printf((log) => `${log.timestamp} [${log.level}]: ${log.message}`)
+    )
+}));
+
+//Logging on the Mongo-db database. 
+allowedTransport.push(new winston.transports.MongoDB({
+    level: 'error',
+    db: LOG_DB_URL,
+    collection: 'logs'
+}))
+
+allowedTransport.push(new winston.transports.File({
+    filename: `app.log`
+}))
+
+const logger = winston.createLogger({
+    //format
+    format: winston.format.combine(
+        // first agrument to combine is defining how we want the timestamp to come-up 
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH-mm-ss'
+        }),
+        //Second agrument to combine method, which define what is exactly goint to printed in log
+        winston.format.printf((log) => `${log.timestamp} [${log.level.toUpperCase()}]: ${log.message}`)
+    ),
+    transports: allowedTransport
+});
+
+module.exports = logger
+
--- src/config/server.config.js
+++ src/config/server.config.js
@@ -4,5 +4,6 @@ dotenv.config()
 module.exports = {
     PORT: process.env.PORT,
     ATLAS_DB_URL: process.env.ATLAS_DB_URL,
-    NODE_ENV: process.env.NODE_ENV ||"DEVELOPMENT"
+    LOG_DB_URL: process.env.LOG_DB_URL,
+    NODE_ENV: process.env.NODE_ENV || "DEVELOPMENT"
 }
